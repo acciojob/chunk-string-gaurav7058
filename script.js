3dfbc454e6344428112f3f1f31f59a7e2d1a5325
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-07 14:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ function stringChop(str, chunkLength) {
         return [];
     }
 
-    // Initialize an empty array to hold the chunks
     const chunks = [];
-
-    // Loop through the string and create chunks
     for (let i = 0; i < str.length; i += chunkLength) {
-        // Slice the string from the current index to the current index + chunk length
         chunks.push(str.slice(i, i + chunkLength));
     }
 
@@ -18,4 +14,4 @@ function stringChop(str, chunkLength) {
 // Do not change the code below
 const str = prompt("Enter String."); 
 const size = parseInt(prompt("Enter Chunk Size."), 10);
-alert(JSON.stringify(stringChop(str, size)));
+alert(stringChop(str, size).join(','));
